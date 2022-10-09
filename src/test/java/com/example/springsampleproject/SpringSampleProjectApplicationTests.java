ea3e7f668a93a5771817d4dc4399efb1d0760732
pasanyasaras <pasansmart@gmail.com>
2022-10-09 17:29:51 +0530

    Initial commit

---
+++ src/test/java/com/example/springsampleproject/SpringSampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
